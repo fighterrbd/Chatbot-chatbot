20c410874e5214abd07e49d4fbc537dc9dfb817b
fighterrbd <fighterrbd@gmail.com>
2025-05-19 23:34:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = 6435225
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7833972330:AAErp7yd_GYU9UyvE84tyfvFlb0KctRe9do")
+BOT_TOKEN = getenv("BOT_TOKEN", "7442570619:AAGDHJtxnMX17aoE9au4HD_Zo11a7fWCQxg")
 STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", 7668520999))
-SUPPORT_GRP = "RIYA_chat_SUPPORT"
-UPDATE_CHNL = "riya_NETWORK"
-OWNER_USERNAME = "ll_hunter_lll"
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://DxLEGEND143:DxLEGEND143@dxlegend.oztipqk.mongodb.net/?retryWrites=true&w=majority&appName=DxLEGEND")
+OWNER_ID = int(getenv("OWNER_ID", 7530396550))
+SUPPORT_GRP = "Jani_RP"
+UPDATE_CHNL = "Jani_RP"
+OWNER_USERNAME = "Jani_RP_Lover"
